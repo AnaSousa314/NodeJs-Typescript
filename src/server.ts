539d7e2145b8f89e5a5cf63b658680848c664761
Ana.Sousa <anasousa314@outlook.com>
2021-08-27 16:16:22 -0300

    aula13 e 14 sobre teoria e trabalho com rotas

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,19 @@ const server = express();
 
 server.get('/',(req:Request,res:Response)=>{
   res.send("Olá Mundo")
-})
+});
+
+server.get('/noticia/:slug',(req:Request,res:Response)=>{
+  const {slug} = req.params;
+  
+  res.send(`Noticia: ${slug}`);
+});
+
+server.get('/voo/:origem-/:destino',(req:Request,res:Response)=>{
+  const {origem,destino} = req.params;
+
+  res.send(`A origem é ${origem.toUpperCase()} e destino ${destino.toUpperCase()}`);
+});
 
 
 server.listen(3000,()=>console.log('Server Started'));
