4131db4e88114fb173cc7f9093db089714a18514
Ana.Sousa <anasousa314@outlook.com>
2021-08-27 15:53:09 -0300

    aula09 a 11 sobre importacao de arquivos externos,nodemon e nodemon com typescript

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // const Matematica = require('./Matematica');
 import {somar,multiplicar,subtrair} from './Matematica'
-
+import validator from 'validator'
 // ou
 
 // import * as Matematica from './Matematica'
@@ -16,6 +16,7 @@ let n2:number = 2;
 
 
 console.log(`SOMA: ${somar(n1,n2)}`);
-console.log(`SUBTRAÇÃO: ${subtrair(n1,n2)}`)
-console.log(`MULTIPLICAÇÃO: ${multiplicar(n1,n2)}`)
+console.log(`SUBTRAÇÃO: ${subtrair(n1,n2)}`);
+console.log(`MULTIPLICAÇÃO: ${multiplicar(n1,n2)}`);
 
+console.log(validator.isEmail('ana@email.com'))
