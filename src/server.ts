f0c9b772d8a412ac5658421bc4f14827cf07c7d3
Ana.Sousa <anasousa314@outlook.com>
2021-08-27 16:41:55 -0300

    aula16 Criando o 404 pagina nao encontrada

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
-import express from 'express';
+import express, {Response,Request} from 'express';
 import mainRoutes from './routes/index';
-import painelRoutes from './routes/painel'
+
 const server = express();
 
 server.use(mainRoutes);
 
-//esse primeiro parametro do .use é um prefixo para distinguir de onde vem a rota, arquivos de rotas diferentes não podem ter o mesmo prefixo
-server.use('/painel',painelRoutes);
+
+//deve ser feito no servidor
+server.use((req:Request,res:Response)=>{
+  res.status(404).send('Página não encontrada')
+})
 
 server.listen(3000,()=>console.log('Server Started'));
 
