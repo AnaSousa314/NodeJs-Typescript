91f1461bf2e9cbd79e42e09af32c0ef66713c62c
Ana.Sousa <anasousa314@outlook.com>
2021-08-30 15:31:45 -0300

    aula17 a 19 sobre pasta publica, template engines e mustache

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import {Router,Response,Request} from 'express'
 const router = Router();
 
 router.get('/',(req:Request,res:Response)=>{
-  res.send("Olá Mundo")
+  res.render('home');
 });
 
 router.get('/contato',(req:Request,res:Response)=>{
